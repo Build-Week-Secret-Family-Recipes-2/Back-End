c8ffbed16af25f9344d423500146674d80e5f3ef
Asha <ashalbenjamin@gmail.com>
2020-06-22 23:50:22 -0500

    auth update

--- recipies/recipe-model.js
+++ recipies/recipe-model.js
@@ -1,24 +1,14 @@
 const db = require('../database/dbConfig');
 
 module.exports = {
-<<<<<<< HEAD
 	findAllRecipes,
 	findRecipeById,
 	addRecipe,
 	findRecipeBy,
 	updateRecipe,
 	eraseRecipe,
+	findRecipeByUserId,
 };
-=======
-    findAllRecipes,
-    findRecipeById,
-    addRecipe,
-    findRecipeBy,
-    updateRecipe,
-    eraseRecipe,
-    findRecipeByUserId
-}
->>>>>>> 5b5923ba43a5461ca65977756be7d73a800cfca2
 
 //resolves to an array of users
 function findAllRecipes() {
@@ -30,32 +20,18 @@ function findRecipeBy(filter) {
 }
 
 //resolves to  a single user or null
-<<<<<<< HEAD
 function findRecipeById(id) {
-	return db('recipes').where({ id }).first();
+	return db('recipes').where({ id });
+}
+
+//resolves to  a single user or null
+function findRecipeByUserId(userId) {
+	return db('recipes').where({ userId }).first();
 }
 
 async function addRecipe(recipe) {
 	const [id] = await db('recipes').insert(recipe);
 	return findById(id);
-=======
-function findRecipeById(id){
-    return db('recipes').where({id});
-
-}
-
-//resolves to  a single user or null
-function findRecipeByUserId(userId){
-    return db('recipes').where({userId}).first()
-
-}
-
-
-async function addRecipe(recipe){
-    const [id] = await db('recipes').insert(recipe);
-    return findById(id);
-
->>>>>>> 5b5923ba43a5461ca65977756be7d73a800cfca2
 }
 
 function updateRecipe(data, id) {
