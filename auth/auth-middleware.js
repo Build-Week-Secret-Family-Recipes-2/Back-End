00b40ddceb591a8ea6047be4de48d699ddd1c86b
Kevin Silvestre <kevsilvo@gmail.com>
2020-06-25 22:06:22 -0400

    changes to header

--- auth/auth-middleware.js
+++ auth/auth-middleware.js
@@ -1,12 +1,12 @@
 const jwt = require('jsonwebtoken');
 
 module.exports = (req, res, next) => {
-	var token = req.headers['Authorization'];
+	const { authorization } = req.headers
 
-	if(token) {
+	if(authorization) {
 		const secret = process.env.JWT_SECRET || 'it is a secret, is it safe?';
 
-		jwt.verify(token, secret, (err, decodedToken) => {
+		jwt.verify(authorization, secret, (err, decodedToken) => {
 			if (err) {
 				res.status(401).json({ message: 'Invalid Token' });
 			} else {
@@ -15,6 +15,7 @@ module.exports = (req, res, next) => {
 			}
 		});
 	} else {
+		
 		res.status(401).json({ message: 'Please login and try again' });
 	}
 };
